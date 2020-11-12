c6ef5e7582723388a6cdf8be5e64675f6c34a24b
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2020-11-12 12:32:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//White spaces just matter if inside quotation marks
+
+var tech = 'JS';
+console.log("I'm coding " + tech + " :)");
+
+/*
+Variables
+Syntax: var/let/const variable_name=value
+-after var/let/const we have to put space
+-after variable name we can put space or not. It's not a must
+-Equal symbol is a must and we can not use anything else instead
+-after equal symbol we can put space or not. It's not a must
+-We can create variable without any value. In this case we should not use equal mark
+-If we use equal mark we have to set value;
+*/
+var myVar; //we did not INITIAL value. So this is an empty variable
+var style = 'my style'; //We set INITIAL value
+console.log(myVar);
+myVar = 'Anymore I have a value too'; //We set the value here to the variable
+
+console.log(myVar);
+
